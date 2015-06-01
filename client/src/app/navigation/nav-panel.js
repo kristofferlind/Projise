2f069079d41141a4b6c33f215bc47252339cf062
kristofferlind <kristoffer@krad.se>
2015-06-01 13:06:34 +0200

    feat: only show usable menu options

--- client/src/app/navigation/nav-panel.js
+++ client/src/app/navigation/nav-panel.js
@@ -6,12 +6,36 @@ var React = require('react/addons'),
     Navbar = require('react-bootstrap').Navbar,
     Accordion = require('react-bootstrap').Accordion,
     Panel = require('react-bootstrap').Panel,
-    RequireAuthentication = require('../require-authentication');
+    RequireAuthentication = require('../require-authentication'),
+    ProjectStore = require('../project/project.store'),
+    SprintStore = require('../sprint/sprint.store'),
+    SprintInteractions = require('../sprint/sprint.interactions');
 
 require('./nav-panel.scss');
 
 var NavPanel = React.createClass({
     mixins: [RequireAuthentication],
+    getInitialState: function() {
+        return {
+            projectId: ProjectStore.getActiveProjectId(),
+            sprintId: SprintStore.getActiveSprintId()
+        };
+    },
+    componentDidMount: function() {
+        ProjectStore.addChangeListener(this.onChange);
+        SprintStore.addChangeListener(this.onChange);
+        SprintInteractions.loadAll();
+    },
+    componentWillUnmount: function() {
+        ProjectStore.removeChangeListener(this.onChange);
+        SprintStore.removeChangeListener(this.onChange);
+    },
+    onChange: function() {
+        this.setState({
+            projectId: ProjectStore.getActiveProjectId(),
+            sprintId: SprintStore.getActiveSprintId()
+        });
+    },
     render: function() {
         var component = this;
         var getClassName = function() {
@@ -21,16 +45,26 @@ var NavPanel = React.createClass({
                 return 'pane';
             }
         };
+
+        var navItems = [];
+        navItems.push(<NavItem eventKey={1} href="#/manage-projects"><i className="fa fa-2x fa-archive"></i> Manage projects</NavItem>);
+        navItems.push(<NavItem eventKey={2} href="#/manage-teams"><i className="fa fa-2x fa-users"></i> Manage teams</NavItem>);
+
+        if (this.state.projectId) {
+            navItems.push(<NavItem eventKey={4} href="#/plan-project"><i className="fa fa-2x fa-puzzle-piece"></i> Plan project</NavItem>);
+            navItems.push(<NavItem eventKey={2} href="#/manage-documents"><i className="fa fa-2x fa-file-text"></i> Manage documents</NavItem>);
+            // navItems.push(<NavItem eventKey={3} href="#/"><i className="fa fa-2x fa-dashboard"></i> Project overview</NavItem>);
+        }
+
+        if (this.state.sprintId) {
+            navItems.push(<NavItem eventKey={5} href="#/plan-sprint"><i className="fa fa-2x fa-coffee"></i> Plan sprint</NavItem>);
+            navItems.push(<NavItem eventKey={6} href="#/my-story"><i className="fa fa-2x fa-thumb-tack"></i> My story</NavItem>);
+        }
+
         return (
             <section id="nav-panel" className={getClassName()}>
                 <Nav className="sidebar-nav">
-                    <NavItem eventKey={1} href="#/manage-projects"><i className="fa fa-2x fa-archive"></i> Manage projects</NavItem>
-                    <NavItem eventKey={2} href="#/manage-teams"><i className="fa fa-2x fa-users"></i> Manage teams</NavItem>
-                    {/*<NavItem eventKey={3} href="#/"><i className="fa fa-2x fa-dashboard"></i> Project overview</NavItem>*/}
-                    <NavItem eventKey={4} href="#/plan-project"><i className="fa fa-2x fa-puzzle-piece"></i> Plan project</NavItem>
-                    <NavItem eventKey={2} href="#/manage-documents"><i className="fa fa-2x fa-file-text"></i> Manage documents</NavItem>
-                    <NavItem eventKey={5} href="#/plan-sprint"><i className="fa fa-2x fa-coffee"></i> Plan sprint</NavItem>
-                    <NavItem eventKey={6} href="#/my-story"><i className="fa fa-2x fa-thumb-tack"></i> My story</NavItem>
+                    {navItems}
                 </Nav>
             </section>
         );
